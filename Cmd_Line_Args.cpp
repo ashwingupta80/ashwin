02eac544be4b73132e71b320890c62e174a777a2
ashwingupta80 <36362610+ashwingupta80@users.noreply.github.com>
2018-02-11 15:36:49 +0530

    Create Cmd_Line_Args.cpp

---
+++ Cmd_Line_Args.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main (int args_count, char *args[]) {
+
+for (int x; x<args_count; x++) {
+          cout << args[x];
+        }
+
+    }
